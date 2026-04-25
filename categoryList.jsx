dffff9881b3fe7b2391247e6c147a5f25c7cce46
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-04-25 15:25:05 +0300

    Add CategoryList component to render categories

---
+++ categoryList.jsx
@@ -0,0 +1,11 @@
+import Category from './Category.jsx';
+export default function CategoryList(props) {
+  const { categories } = props;
+  return (
+    <>
+      {categories.map((item, index) => (
+        <Category category={item} key={index} />
+      ))}
+    </>
+  );
+}
